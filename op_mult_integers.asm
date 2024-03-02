291f3dec0bb93058ee077af90656b2bd12cc8e30
mihkuno <caindayjoeninyo@gmail.com>
2024-03-02 12:56:09 +0800

    added mul integers

--- op_mult_integers.asm
+++ op_mult_integers.asm
@@ -2,6 +2,11 @@
 
 .text
 
+	# 3 ways to multiply
+	# 	mul - 3 registers with overflow
+	# 	mult - 2 registers, without overflow (uses hi and lo registers) 
+	# 	sll - shift left logical, efficient but less flexibility
+
 	# (s0-s7 function save and restore) 
 	# (t0-t7 not need to save )
 	
